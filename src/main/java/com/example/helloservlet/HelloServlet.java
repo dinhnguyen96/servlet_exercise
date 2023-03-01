bda478c469713c0dd7182c4ae6716e2ae0afbe71
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-03-01 10:40:07 +0700

    add date time servlet exercise

---
+++ src/main/java/com/example/helloservlet/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.example.helloservlet;
+
+import java.io.*;
+import java.time.LocalDate;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name ="Datetime-Servlet", value = "/datetime-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void init()
+    {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.setContentType("text/html");
+        LocalDate now = LocalDate.now();
+        // response to client
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        printWriter.println("<body>");
+        printWriter.println("<h2>"+now+"</h2>");
+        printWriter.println("</body>");
+        printWriter.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
